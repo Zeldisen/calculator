437e59c1cf6dc90b9ec1eaed8ec0b7242df262cf
emiliaforsheim <emiliaforsheim@outlook.com>
2024-10-18 11:24:18 +0200

    added to calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = 0;
+        int number2 = 0;
+
+        number1 = scanner.nextInt();
+        number2 = scanner.nextInt();
+
+        addition(number1,number2);
+        subtraction(number1, number2);
+        multiplication(number1, number2);
+        division(number1, number2);
+
+    }
+    public static void addition(int number1,int number2){
+        int result = number1 + number2;
+        System.out.println(result);
+
+    }
+
+    public static void subtraction(int number1, int number2) {
+        int result = number1 - number2;
+        System.out.println(result);
+    }
+
+    public static void multiplication(int number1, int number2) {
+        int result = number1 * number2;
+        System.out.println(result);
+    }
+
+    public static void division(int number1, int number2) {
+        int result = number1 / number2;
+        System.out.println(result);
     }
 }
