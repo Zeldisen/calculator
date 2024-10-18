00258369ddc8d74833c23bb69e4bd9f6e6832ad2
Nettan <jeanette.norberg82@gmail.com>
2024-10-18 10:45:41 +0200

    added addition method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = 0;
+        int number2 = 0;
+
+        number1 = scanner.nextInt();
+        number2 = scanner.nextInt();
+
+        addition(number1,number2);
+
+    }
+    public static void addition(int number1,int number2){
+        int numbers = number1 + number2;
+        System.out.println(numbers);
+
     }
 }
