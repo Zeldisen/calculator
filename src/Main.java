38130bf8d5615fc364b468c9784db87d70d4e958
camillafalk <ccamilla.falk@gmail.com>
2024-10-18 10:53:09 +0200

    Added methods: multiply, subtraction, division

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,25 @@ public class Main {
         number2 = scanner.nextInt();
 
         addition(number1,number2);
+        subtraction(number1,number2);
+        multiply(number1,number2);
+        division(number1,number2);
 
     }
     public static void addition(int number1,int number2){
         int numbers = number1 + number2;
         System.out.println(numbers);
-
+    }
+    public static void subtraction(int number1,int number2){
+        int numbers = number1 - number2;
+        System.out.println(numbers);
+    }
+    public static void multiply(int number1,int number2){
+        int numbers = number1 * number2;
+        System.out.println(numbers);
+    }
+    public static void division(int number1,int number2){
+        double divisionSum = (double) number1 / number2;
+        System.out.println(divisionSum);
     }
 }
