52496d9cf77a7de3f2458ca15c693b4f6eef6055
Kseniia <anillinnittakorall@gmail.com>
2024-10-18 16:01:43 +0700

    Kseniias commit

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,30 @@ public class Main {
         number1 = scanner.nextInt();
         number2 = scanner.nextInt();
 
-        addition(number1,number2);
-
+        addition(number1, number2);
+        substraction(number1, number2);
+        multiplication(number1, number2);
+        division(number1, number2);
     }
-    public static void addition(int number1,int number2){
+
+    public static void addition(int number1, int number2) {
         int numbers = number1 + number2;
         System.out.println(numbers);
 
+    }
+
+    public static void substraction(int number1, int number2) {
+        int substractionNumbers = number1 - number2;
+        System.out.println(substractionNumbers);
+    }
+
+    public static void multiplication(int number1, int number2) {
+        int multiplicationNumbers = number1 * number2;
+        System.out.println(multiplicationNumbers);
+    }
+
+    public static void division(int number1, int number2) {
+        int divisionNumbers = number1 / number2;
+        System.out.println(divisionNumbers);
     }
 }
