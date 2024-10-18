a04b8559c2089f084e7964f0ec9769d4e57b5e64
Kseniia <anillinnittakorall@gmail.com>
2024-10-18 17:14:39 +0700

    added simple switch menu without debugging

--- src/Main.java
+++ src/Main.java
@@ -7,39 +7,64 @@ public class Main {
 
         int number1 = 0;
         int number2 = 0;
-
+        System.out.println("Enter the first number:");
         number1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the second number:");
         number2 = scanner.nextInt();
 
+        System.out.println("Thank you! Choose one of the options:");
+        System.out.println("1. Addition.");
+        System.out.println("2. Subtraction.");
+        System.out.println("3. Multiplication.");
+        System.out.println("4. Division.");
 
-        addition(number1, number2);
-        subtraction(number1, number2);
-        multiplication(number1, number2);
-        division(number1, number2);
+        int userAnswer = scanner.nextInt();
+        switch (userAnswer) {
+            case 1:
+                System.out.print(number1 + "+" + number2 + "=");
+                addition(number1, number2);
+                break;
+            case 2:
+                System.out.print(number1 + "-" + number2 + "=");
+                subtraction(number1, number2);
+                break;
+            case 3:
+                System.out.print(number1 + "*" + number2 + "=");
+                multiplication(number1, number2);
+                break;
+            case 4:
+                System.out.print(number1 + "/" + number2 + "=");
+                division(number1, number2);
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
     }
 
-        public static void addition ( int number1, int number2){
-            int result = number1 + number2;
-            System.out.println(result);
-        }
+    public static void addition(int number1, int number2) {
+        int result = number1 + number2;
+        System.out.println(result);
+    }
 
 
-        public static void subtraction ( int number1, int number2){
-            int result = number1 - number2;
-            System.out.println(result);
-        }
+    public static void subtraction(int number1, int number2) {
+        int result = number1 - number2;
+        System.out.println(result);
+    }
 
-        public static void multiplication ( int number1, int number2){
-            int result = number1 * number2;
-            System.out.println(result);
-        }
+    public static void multiplication(int number1, int number2) {
+        int result = number1 * number2;
+        System.out.println(result);
+    }
 
-        public static void division ( int number1, int number2){
-            double result = (double) number1 / number2;
-            System.out.println(result);
-
-
-        }
+    public static void division(int number1, int number2) {
+        double result = (double) number1 / number2;
+        System.out.println(result);
 
 
     }
+
+
+}
